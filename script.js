13b94d6a027e944b819d582497457aa079669956
Kyrylo <kirilltaranskyi@gmail.com>
2023-04-27 15:58:09 +0300

    samples added

---
+++ script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById('post_id');
+const postContainer = document.getElementById('post_container');
+const commentsBtn = document.getElementById('comments_btn');
+const commentsContainer = document.getElementById('comments_container');
+
