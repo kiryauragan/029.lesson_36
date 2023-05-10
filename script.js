af54760c53f7b06199b58b48b2bc611785eee541
Kyrylo <kirilltaranskyi@gmail.com>
2023-05-10 12:51:37 +0300

    changes 1st try

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const input = document.getElementById('post_id');
-const postContainer = document.getElementById('post_container');
-const commentsBtn = document.getElementById('comments_btn');
+const postButton = document.getElementById('post_button');
+const postBody = document.getElementById('post_body');
 const commentsContainer = document.getElementById('comments_container');
+
+postButton.addEventListener('click', () => {
+    const postId = parseInt(input.value);
+    if (post_id < 1 || post_id > 100) {
+      alert('Айді поста має бути в межах від 1 до 100 включно!');
+      return;
+    }
+    
+    
+    fetch ('https://jsonplaceholder.typicode.com/posts/${post_id}')
+    .then((response) => {
+        return response.json
+    })
+    .then((data) => {
+        console.log(data)
+    });
+
+});
+
 
